2fe332592d53449d027ef2d72102f7316a29e5f4
Orest Savyk <orestsavyk@MacBook-Pro-von-Orest.local>
2022-01-14 16:06:04 +0200

    Generate Shopify component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShopifyComponent } from './component/shopify/shopify.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopifyComponent
   ],
   imports: [
     BrowserModule
