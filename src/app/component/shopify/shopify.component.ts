b665dc59a91e9d5e014577431234c75ce0335406
Orest Savyk <orestsavyk@MacBook-Pro-von-Orest.local>
2022-01-17 18:41:13 +0200

    Improve + Clean code

--- src/app/component/shopify/shopify.component.ts
+++ src/app/component/shopify/shopify.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { COOPERATION_LOGOS_ITEMS, SERVICES_ITEMS } from './shopify.constant';
 
 @Component({
   selector: 'app-shopify',
   templateUrl: './shopify.component.html',
-  styleUrls: ['./shopify.component.scss']
+  styleUrls: ['./shopify.component.scss'],
 })
 export class ShopifyComponent implements OnInit {
+  ServicesItems: any[] = SERVICES_ITEMS;
+  CooperationLogosItems: any[] = COOPERATION_LOGOS_ITEMS;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
